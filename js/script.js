f65812e9f48e8270ddbf12fd37581282b65fce23
Shugmi Shumunov <SShumunov@gmail.com>
2016-07-23 19:14:45 -0400

    Hide buttons and titles on app start.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,25 @@
 $(document).ready(function(){
   var buzzer = $("#buzzer")[0];
   var session = parseInt($("#sessionTime").html());
+  var count = parseInt($("#sessionTime").html());
   var breakT = parseInt($("#breakTime").html());
-  buzzer.play();
+
   $("#reset").hide();
 
+  $("#startButton").on("click", function(){
+    var counter = setInterval(timer, 1000);
+    function timer (){
+      // Hide Vars
+      $("#startButton, #minus5Clock, #add5Clock, #minus5Break, #add5Break, #breakTime, #sessionHeader, #breakHeader").hide();
+      $("#timeType").html("Session Time:");
+      count -=1;
+      if(count===0){
+        buzzer.play();
+        clearInterval(counter);
+      }
+      $("#sessionTime").html(count);
+    }
+  });
   $("#minus5Clock").on("click",function(){
     if(session>5){
       session -= 5;
