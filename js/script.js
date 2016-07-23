e7d340f2931a26c4bf1ff02f55c0b0741a4a2e54
Shugmi Shumunov <SShumunov@gmail.com>
2016-07-23 19:25:14 -0400

    Add break timer to function scope.

--- js/script.js
+++ js/script.js
@@ -9,15 +9,26 @@ $(document).ready(function(){
   $("#startButton").on("click", function(){
     var counter = setInterval(timer, 1000);
     function timer (){
-      // Hide Vars
+      // Hide all the different titles and buttons
       $("#startButton, #minus5Clock, #add5Clock, #minus5Break, #add5Break, #breakTime, #sessionHeader, #breakHeader").hide();
-      $("#timeType").html("Session Time:");
+      $("#timeType").html("Session Time: ");
       count -=1;
       if(count===0){
         buzzer.play();
         clearInterval(counter);
+        var startBreak = setInterval (breakTimer, 1000);
+        $("#sessionTime").hide();
       }
       $("#sessionTime").html(count);
+      function breakTimer(){
+        $("#timeType").html("Break Time: ");
+        $("#breakTime").show();
+        breakT -=1;
+        if(breakT===0){
+          clearInterval(startBreak);
+        }
+          $("#breakTime").html(breakT);
+      }
     }
   });
   $("#minus5Clock").on("click",function(){
