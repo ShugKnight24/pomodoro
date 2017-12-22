d6a18aaf7483a6602c621dd413806746cc45568f
Shugmi Shumunov <SShumunov@gmail.com>
2017-12-22 17:14:23 -0500

    Add spaces between mathematical operators

--- js/script.js
+++ js/script.js
@@ -9,49 +9,53 @@ $(document).ready(function(){
 	$('#reset').hide();
 
 	$('#startButton').on('click', function(){
+
 		var counter = setInterval(timer, 1000);
-		session*=60;
+
+		session *= 60;
+
 		function timer (){
 			// Hide all the different titles and buttons
 			$('#startButton, #minus5Clock, #add5Clock, #minus5Break, #add5Break, #breakTime, #sessionHeader, #breakHeader, #description').hide();
 			$('#timeType').show();
 			$('#timeType').html('Session Time: ');
-			session-=1;
-			if(session===0){
+
+			session -= 1;
+			if(session === 0){
 				buzzer.play();
 				clearInterval(counter);
 				var startBreak = setInterval (breakTimer, 1000);
 				$('#sessionTime').hide();
 			}
-			if(session%60>=10){
-				$('#sessionTime').html(Math.floor(session/60)+':'+session%60);
+			if(session % 60 >= 10){
+				$('#sessionTime').html(Math.floor(session / 60) + ':' + session % 60);
 			} else {
-				$('#sessionTime').html(Math.floor(session/60)+':'+'0'+session%60);
+				$('#sessionTime').html(Math.floor(session / 60) + ':' + '0' + session %  60);
 			}
 			function breakTimer(){
 				$('#timeType').html('Break Time: ');
 				$('#breakTime').show();
-				breakT*=60;
+				breakT *= 60;
 				$('#timeType').show();
-				breakT-=1;
-				if(breakT===0){
+				breakT -= 1;
+				if(breakT === 0){
 					clearInterval(startBreak);
 					buzzer.play();
 					$('#reset').show();
 					$('#breakTime, #timeType').hide();
 				}
-				if(breakT%60>=10){
-					$('#breakTime').html(Math.floor(breakT/60)+':'+breakT%60);
+				if(breakT % 60 >= 10){
+					$('#breakTime').html(Math.floor(breakT / 60) + ':' + breakT % 60);
 				} else {
-					$('#breakTime').html(Math.floor(breakT/60)+':'+'0'+breakT%60);
+					$('#breakTime').html(Math.floor(breakT / 60) + ':' + '0' + breakT % 60);
 				}
 			}
 		}
 	});
 
 	$('#reset').on('click', function(){
-		session=25;
-		breakT=5;
+		session = 25;
+		breakT = 5;
 		$('#sessionTime').html(session);
 		$('#breakTime').html(breakT);
 		$('#startButton, #minus5Clock, #add5Clock, #minus5Break, #add5Break, #sessionTime, #breakTime, #sessionHeader, #breakHeader, #description').show();
@@ -59,23 +63,23 @@ $(document).ready(function(){
 	});
 
 	$('#minus5Clock').on('click',function(){
-		if(session>5){
-			session-=5;
+		if(session > 5){
+			session -= 5;
 			$('#sessionTime').html(session);
 		}
 	});
 	$('#add5Clock').on('click',function(){
-		session+=5;
+		session += 5;
 		$('#sessionTime').html(session);
 	});
 	$('#minus5Break').on('click',function(){
-		if(breakT>5){
-			breakT-=5;
+		if(breakT > 5){
+			breakT -= 5;
 			$('#breakTime').html(breakT);
 		}
 	});
 	$('#add5Break').on('click',function(){
-		breakT+=5;
+		breakT += 5;
 		$('#breakTime').html(breakT);
 		});
 });
